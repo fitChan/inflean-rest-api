b56b205bdc500a7e477ca4478b9cdb431093efa6
fitChan <84306157+fitChan@users.noreply.github.com>
2021-08-17 19:17:51 +0900

    Initial commit

---
+++ src/test/java/com/gisunlecture/infleanrestapi/InfleanRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gisunlecture.infleanrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfleanRestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
